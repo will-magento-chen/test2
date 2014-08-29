6663386409999901afdcfd1a17cf8fc43e9f62f2
reedkevin88 <reedkevin88@gmail.com>
2014-08-29 21:16:01 +0400

    Modify seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 puts "Create Contacts"
 20.times do
-  Contact.create({
+  Contact.create!({
                    first_name: Faker::Name.first_name,
                    last_name: Faker::Name.last_name,
                    email: Faker::Internet.email,
