c94221296b7e56f4a96f512e4c5504824c72d441
reedkevin88 <reedkevin88@gmail.com>
2014-08-29 20:32:46 +0400

    Add auto loadpath

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module Barefoot
       g.view_specs false
       g.helper_specs false
     end
+
+    config.autoload_paths += %W(#{config.root}/lib)
+    
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
