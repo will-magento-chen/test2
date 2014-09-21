1acd4dce50effb3d00613b4c02e6d3662275066f
Masahji Stewart <masahji@synctree.com>
2014-09-21 15:13:25 -0700

    using event code instead od ids

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -5,7 +5,7 @@ class ShoppingController < ApplicationController
       @event = Event.find(params[:event_id]) if params[:event_id]
       shopping_context = {}
       shopping_context[:address] = params[:address] unless params[:address].blank?
-      shopping_context[:event_id] = @event.id if @event
+      shopping_context[:event_code] = @event.affiliate_code if @event
       shopping_context[:order_type] = params[:order_type]
       cookies[:bf_shop_ctx] = shopping_context.to_json
     end
