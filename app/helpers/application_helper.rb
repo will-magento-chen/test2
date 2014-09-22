9d5cb1fc9c3d4ce50af88dc903a65b4970c5c0bb
Masahji Stewart <masahji@synctree.com>
2014-09-22 06:27:00 -0700

    Adding sign in and sign out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def magento_url(action = '')
     ENV['BAREFOOT_MAGENTO_URL'] + '/' + action
   end
+
+  def barefoot_sign_in_url
+    ENV['BAREFOOT_SIGN_IN_URL']
+  end
+
+  def barefoot_sign_out_url
+    ENV['BAREFOOT_SIGN_OUT_URL']
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/sign_in'   => 'root#login'
+  get 'users/sign_out'  => 'root#logout'
   devise_for :users
   root to: "contacts#index"
 
